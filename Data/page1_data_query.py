c182c604f6d9e6165932adc81d3321b39b3c7a76
szzuo <112144936+szzuo@users.noreply.github.com>
2023-05-02 12:34:05 +1000

    Add files via upload

--- Data/page1_data_query.py
+++ Data/page1_data_query.py
@@ -1,6 +1,6 @@
 import couchdb.design
 from datetime import datetime
-import pandas as pd
+import json
 import couchdb
 
 # authentication
@@ -14,23 +14,25 @@ if db in couch:
     db = couch[db]
 
 # query data from couchDB
-data_out = {'sentiment' : [], 'loc' : [], 'datetime' : [], 'city' : []}
-for i in range(len(city_names)):
-    city_name = city_names[i]
-    view_results = db.view(f"_design/my_design_page1/_view/gcc_{city_name}")
-    for row in view_results:
-        row = row.value
-        try:
-            if 'sentiment' in list(row.keys()):
-                data_out['sentiment'].append(row['sentiment'])
-                data_out['loc'].append(row['loc'])
-                data_out['city'].append(row['city'])
-                format = "%Y-%m-%dT%H:%M:%S"
-                date = datetime.strptime(row['datetime'][:-5], format)
-                data_out['datetime'].append(date)
-        except KeyError and ValueError:
-            # Ignore the KeyError and continue with the next iteration of the loop
-            continue
-df = pd.DataFrame(data_out)
-df.to_csv(f'page1_data.csv')
+
+with open("page1_data.json", "w") as outfile:
+    for i in range(len(city_names)):
+        data_out = {'sentiment': [], 'loc': [], 'datetime': [], 'city': []}
+        city_name = city_names[i]
+        view_results = db.view(f"_design/my_design_page1/_view/gcc_{city_name}")
+        for row in view_results:
+            row = row.value
+            try:
+                if 'sentiment' in list(row.keys()):
+                    data_out['sentiment'] = row['sentiment']
+                    data_out['loc'] = row['loc']
+                    data_out['city'] = row['city']
+                    format = "%Y-%m-%dT%H:%M:%S"
+                    date = datetime.strptime(row['datetime'][:-5], format)
+                    data_out['datetime'] = date
+            except KeyError and ValueError:
+                # Ignore the KeyError and continue with the next iteration of the loop
+                continue
+        outfile.write(json.dumps(data_out, ensure_ascii=False))
+        outfile.write('\n')
 print(f'file done!')
