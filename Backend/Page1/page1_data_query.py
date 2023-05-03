dca5b4661ec97a28003044a3270de38954a8345f
RoRobinh98 <houjiazhe98@163.com>
2023-05-03 18:06:31 +1000

    add backup

--- Backend/Page1/page1_data_query.py
+++ Backend/Page1/page1_data_query.py
@@ -3,6 +3,8 @@ from datetime import datetime
 import pandas as pd
 import couchdb
 import json
+import os
+
 # authentication
 admin = 'admin'
 password = 'password123456'
@@ -15,26 +17,42 @@ if db in couch:
 
 
 def query_data():
-    data_list = []
-    counter = 0
+    # Get the current file path
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    json_file_path = os.path.join(current_path, 'page1_data.json')
 
-    for i in range(len(city_names)):
-        city_name = city_names[i]
-        view_results = db.view(f"_design/my_design_page1/_view/gcc_{city_name}")
+    # Check if the file exists
+    if os.path.exists(json_file_path):
+        # If the file exists, load the data from the file
+        with open(json_file_path, 'r') as infile:
+            data_list = json.load(infile)
+            print(f'Data loaded from file. {len(data_list)} lines in total.')
+            return data_list
+    else:
+        # If the file does not exist, fetch the data and store it in the file
+        data_list = []
+        counter = 0
 
-        for row in view_results:
-            data_out = {}
-            row = row.value
+        for i in range(len(city_names)):
+            city_name = city_names[i]
+            view_results = db.view(f"_design/my_design_page1/_view/gcc_{city_name}")
 
-            if 'sentiment' in list(row.keys()):
-                data_out['sentiment'] = row['sentiment']
-                data_out['loc'] = row['loc']
-                data_out['city'] = row['city']
-                data_out['datetime'] = row['datetime'][:-5]
+            for row in view_results:
+                data_out = {}
+                row = row.value
 
-                data_list.append(data_out)
-                counter += 1
+                if 'sentiment' in list(row.keys()):
+                    data_out['sentiment'] = row['sentiment']
+                    data_out['loc'] = row['loc']
+                    data_out['city'] = row['city']
+                    data_out['datetime'] = row['datetime'][:-5]
 
-    print(f'Data done! {counter} lines in total.')
-    return data_list
+                    data_list.append(data_out)
+                    counter += 1
 
+        # Store the data in the 'page1_data.json' file
+        with open(json_file_path, 'w') as outfile:
+            json.dump(data_list, outfile, ensure_ascii=False)
+
+        print(f'Data done! {counter} lines in total.')
+        return data_list
