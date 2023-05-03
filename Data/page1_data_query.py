55abf70ee4d461a74dde53264a67cc4c167914ab
szzuo <112144936+szzuo@users.noreply.github.com>
2023-05-03 16:51:06 +1000

    Add files via upload

--- Data/page1_data_query.py
+++ Data/page1_data_query.py
@@ -1,8 +1,8 @@
 import couchdb.design
 from datetime import datetime
-import json
+import pandas as pd
 import couchdb
-
+import json
 # authentication
 admin = 'admin'
 password = 'password123456'
@@ -14,25 +14,22 @@ if db in couch:
     db = couch[db]
 
 # query data from couchDB
-
 with open("page1_data.json", "w") as outfile:
+    counter = 0
     for i in range(len(city_names)):
-        data_out = {'sentiment': [], 'loc': [], 'datetime': [], 'city': []}
         city_name = city_names[i]
         view_results = db.view(f"_design/my_design_page1/_view/gcc_{city_name}")
         for row in view_results:
+            data_out = {}
             row = row.value
-            try:
-                if 'sentiment' in list(row.keys()):
-                    data_out['sentiment'] = row['sentiment']
-                    data_out['loc'] = row['loc']
-                    data_out['city'] = row['city']
-                    format = "%Y-%m-%dT%H:%M:%S"
-                    date = datetime.strptime(row['datetime'][:-5], format)
-                    data_out['datetime'] = date
-            except KeyError and ValueError:
-                # Ignore the KeyError and continue with the next iteration of the loop
-                continue
-        outfile.write(json.dumps(data_out, ensure_ascii=False))
-        outfile.write('\n')
-print(f'file done!')
+            if 'sentiment' in list(row.keys()):
+                data_out['sentiment'] = row['sentiment']
+                data_out['loc'] = row['loc']
+                data_out['city'] = row['city']
+                data_out['datetime'] = row['datetime'][:-5]
+                outfile.write(json.dumps(data_out, ensure_ascii=False))
+                outfile.write('\n')
+                counter += 1
+
+print(f'file done! {counter} lines in total.')
+
