26e992aed24bd99e1fd02162a66c0a9358284d85
Robert zr Bai <robertbai98@gmail.com>
2023-05-10 00:10:21 +1000

    page2 data update

--- Backend/Page2/2csv.py
+++ Backend/Page2/2csv.py
@@ -2,14 +2,14 @@ import json
 import csv
 
 # Load the JSON data from a file or a string
-with open('page2_data_Mastodon_single.json', 'r') as f:
+with open('page2_data_M.json', 'r') as f:
     data = json.load(f)
 
 # Extract the keys from the first object in the JSON data
 keys = data[0].keys()
 
 # Open a new CSV file and write the header row
-with open('page2_data_Mastodon_single.csv', 'w', newline='') as f:
+with open('page2_data_M.csv', 'w', newline='') as f:
     writer = csv.DictWriter(f, fieldnames=keys)
     writer.writeheader()
 
--- Backend/Page2/page2_data_query_mastodon_single.py
+++ Backend/Page2/page2_data_query_mastodon_single.py
@@ -4,8 +4,8 @@ import pandas as pd
 import couchdb
 import json
 import os
+import requests
 
-tweet_rank = ['top1', 'top2', 'top3', 'top4', 'top5', 'top6', 'top7', 'top8','top9', 'top10']
 
 
 def query_data(db):
@@ -13,59 +13,80 @@ def query_data(db):
     current_path = os.path.dirname(os.path.abspath(__file__))
     json_file_path = os.path.join(current_path, 'page2_data_M.json')
 
-    # Check if the file exists
-    if os.path.exists(json_file_path):
-        # If the file exists, load the data from the file
-        with open(json_file_path, 'r') as infile:
-            data_list = json.load(infile)
-            print(f'Data loaded from file. {len(data_list)} lines in total.')
-            return data_list
-    else:
+    
+    preurl="http://admin:password123456@172.26.128.137:5984/mastodon2/_design/mas/_view/top?group=true&&reduce=true&descending=true"
+    
+    # Make the HTTP request to the view
+    response = requests.get(preurl)
+
+    # Parse the JSON response
+    result = json.loads(response.text)
+
+    # Print the top 10 authors with the most entries
+
+    sorted_list = sorted(result['rows'], key=lambda x: x['value'], reverse=True)
+
+    top_10_items = sorted_list[:10]
+
+    print(top_10_items)
+
         # If the file does not exist, fetch the data and store it in the file
-        data_list = []
+    data_list = []
+    Aname="i['key']"
+    num_M=0
+    counter = 0
+    rank=10
+    for i in top_10_items:
+        print(i)
+        rank_name = rank
+        rank-=1
+        Aname=i['key']
+        print(Aname)
+        num_M=i['value']
+        view_results = db.view(f"_design/mas/_view/full")
         
-        counter = 0
-        for i in range(len(tweet_rank)):
-            rank_name = tweet_rank[i]
-            view_results = db.view(f"_design/test1/_view/{rank_name}")
-           
-            for row in view_results:
-                data_out = {}
-                # print(rank_name)
-               
-                # print(row.id)
+        for row in view_results:
+            data_out = {}
+            # print(rank_name)
+            if(row.key==Aname):
+            # print(row.id)
                 row = row.value
-                
+            
                 data_out['rank'] = rank_name
-                data_out['author_id'] = row['author_id']
-                
-                data_out['text'] = row['text']
+                data_out['username'] = row['username']
+                data_out['totalM'] = num_M
+                data_out['content'] = row['content']
                 data_out['created_at'] = row['created_at']
                 data_out['date'] = row['created_at'][:10]
-                data_out['time'] = row['created_at'][12:-5]
-                data_out['author_name'] = row['author_name']
-                data_out['url'] = row['url']
-                
+                data_out['time'] = row['created_at'][12:-6]
+                data_out['display_name'] = row['display_name']
+                data_out['favourites_count'] = row['favourites_count']
+                data_out['followers_count'] = row['followers_count']
+                data_out['following_count'] = row['following_count']
+                data_out['reblogs_count'] = row['reblogs_count']
+                data_out['replies_count'] = row['replies_count']
+                data_out['sensitive'] = row['sensitive']
                 data_out['sentiment'] = row['sentiment']
+                data_out['url'] = row['url']
                 data_out['class']="mastodon"
                 
 
                 data_list.append(data_out)
                 counter += 1
 
-        # Store the data in the 'page1_data.json' file
-        with open(json_file_path, 'w') as outfile:
-            json.dump(data_list, outfile, ensure_ascii=False)
+    # Store the data in the 'page1_data.json' file
+    with open(json_file_path, 'w') as outfile:
+        json.dump(data_list, outfile, ensure_ascii=False)
 
-        print(f'Data done! {counter} lines in total.')
-        return data_list
+    print(f'Data done! {counter} lines in total.')
+    return data_list
 
  # authentication
 def login_to_db():
     admin = 'admin'
     password = 'password123456'
     url = f'http://{admin}:{password}@172.26.128.137:5984/'
-    db = 'mastodon'
+    db = 'mastodon2'
     couch = couchdb.Server(url)
     if db in couch:
         db = couch[db]
